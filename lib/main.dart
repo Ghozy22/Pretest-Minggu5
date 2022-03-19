a3dc7a64f3b70557969cab4dbaf3e1271f44bdb3
Ghozy <ghozyarc12@gmail.com>
2022-03-20 06:31:23 +0700

    Pretest6-Minggu5

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,63 @@
-import 'dart:html';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Pass Data',
+      home: halamanPertama(
+        todos: List.generate(20, (i) => Todo('Todo $i', 'A description of what needs to be done for Todo $i'))
+      ),
+    )
+  );
+}
+
+class Todo{
+  final String title;
+  final String description;
+
+  const Todo(this.title, this.description);
+}
+
+class halamanPertama extends StatelessWidget {
+  const halamanPertama({ Key? key, required this.todos }) : super(key: key);
+
+  final List<Todo> todos;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pretest5'),
+        title: const Text('Todos'),
       ),
-      body:  const Center(
-        child: SelectionButton(),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(todos[index].title),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder:(context) => detailScreen(todo: todos[index]),));
+            },
+          );
+        },
       ),
     );
   }
 }
 
-class SelectionButton extends StatelessWidget {
-  const SelectionButton({ Key? key }) : super(key: key);
+class detailScreen extends StatelessWidget {
+  const detailScreen({ Key? key, required this.todo }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _navigate(context);
-      }, child: const Text('Pilihlah Sesuai Keinginan Anda'),
-    );
-  }
-
-  void _navigate(BuildContext context) async {
-    final hasil = await Navigator.push(context, MaterialPageRoute(builder: (context) =>  const halamanKedua(),));
-
-    ScaffoldMessenger.of(context)
-    ..removeCurrentSnackBar()
-    ..showSnackBar(SnackBar(content: Text('$hasil')));
-  }
-
-}
-
-class halamanKedua extends StatelessWidget {
-  const halamanKedua({ Key? key }) : super(key: key);
+  final Todo todo;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Halaman Kedua'),
+        title: const Text('Detail'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(onPressed: (){
-                Navigator.pop(context, 'Selamat Malam');
-              }, child: const Text('Selamat Malam.')),
-            ),
-            Padding(padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(onPressed: (){
-                Navigator.pop(context, 'Semoga Harimu Menyenangkan');
-              }, child: const Text('Semoga Harimu Menyenangkan.')),
-            )
-          ],
-        ),
+      body: Padding(padding: const EdgeInsets.all(16.0)
+      , child: Text(todo.description),
       ),
     );
   }
